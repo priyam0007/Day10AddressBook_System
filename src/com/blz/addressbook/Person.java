db32bd08b2ab4aaec4767c24877d2db02a47dfef
Priya <Priyamadiwalar0007@gmail.com>
2022-02-19 23:37:38 +0530

    Added: Edit Existing Contact

--- src/com/blz/addressbook/Person.java
+++ src/com/blz/addressbook/Person.java
@@ -5,6 +5,7 @@ package com.blz.addressbook;
 		static String firstName,lastName,address,state,email;
 		static int zip;
 		static long phoneNumber;
+		static int j = 0;
 		static Contact [] personDetails = new Contact[10];
 		
 		public static void main(String args[]) 
@@ -23,35 +24,54 @@ package com.blz.addressbook;
 		    operations();
 		}
 		public static void operations() {
-			int j = 0;
 		    System.out.println("Enter your choice");
 		    System.out.println("1)Add contact");
+		    System.out.println("2)Add contact");
 		    int choice = sc.nextInt();
 		    switch(choice) {
 		    	case 1:
-		    			System.out.println("Enter details of person");
-			    		System.out.println("Add first name");
-			    		firstName = sc.next();
-			    		System.out.println("Add last name");
-			    		lastName= sc.next();
-			    		System.out.println("Add address");
-			    		address = sc.next();
-			    		System.out.println("Add state");
-			    		state= sc.next();
-			    		System.out.println("Add zip");
-			    		zip = sc.nextInt();
-			    		System.out.println("Add phone number");
-			    		phoneNumber = sc.nextLong();
-			    		System.out.println("Add email");
-			    		email = sc.next();
-			    		Contact person4 = new Contact(firstName,lastName,address,state,zip,phoneNumber,email);
-			    		personDetails[3]=person4;
-			    		j++;
-				 for(int i = 0; i < 3 + j;i++) {
-					   System.out.println(personDetails[i]);
-				 }
+		    		 contactInfo();
+			    	 j++;
+		    		Contact person4 = new Contact(firstName,lastName,address,state,zip,phoneNumber,email);
+		    		personDetails[3]=person4;
+		    		System.out.println("Newly updated contact list is");
+			        for(int i = 0; i < 3 + j;i++) {
+				    System.out.println(personDetails[i]);
+			 }
+		    	case 2:
+		    		int i;
+		    		System.out.println("Enter name of person you want to edit");
+		    		String keyName = sc.next();
+		    		for( i = 0; i < 3; i++) {
+		    		if(keyName.equals(personDetails[i].firstName)) {
+		    				contactInfo();
+		    				Contact person5 = new Contact(firstName,lastName,address,state,zip,phoneNumber,email);
+		    		 		personDetails[i]= person5;
+		    			}
+		    		}
+		    		System.out.println("Newly edited contact list is");
+		    		for(i = 0; i < 3;i++) {
+		    			   System.out.println(personDetails[i]);
+		    		}
+		    		
 			}
 		}
+		public static void contactInfo() {
+			System.out.println("Enter details of person");
+			System.out.println("Add first name");
+			firstName = sc.next();
+			System.out.println("Add last name");
+			lastName= sc.next();
+			System.out.println("Add address");
+			address = sc.next();
+			System.out.println("Add state");
+			state= sc.next();
+			System.out.println("Add zip");
+			zip = sc.nextInt();
+			System.out.println("Add phone number");
+			phoneNumber = sc.nextLong();
+			System.out.println("Add email");
+			email = sc.next();
+		}
 	}
-
-
+		 
