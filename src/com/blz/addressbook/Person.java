c5d4e341c8f8807b1f894a3e43f9f40b5134bf53
priyam0007 <94181941+priyam0007@users.noreply.github.com>
2022-02-23 20:47:35 +0530

    Update Person.java

--- src/com/blz/addressbook/Person.java
+++ src/com/blz/addressbook/Person.java
@@ -9,9 +9,9 @@ package com.blz.addressbook;
 		
 		public static void main(String args[]) 
 		{
-			Contact person1 = new Contact("Priya","M","Bijapur","KA",35,8620,"raj@gmail.com");
-			Contact person2 = new Contact("Rishi","R","Chitrudurga","KA",98,9736,"shashi@yahoo.com");
-			Contact person3 = new Contact("Gawrav","G","Durga","KA",47,7620,"amar@edu.com");
+			Contact person1 = new Contact("Priya","M","Bijapur","KA",35,8620,"priya@gmail.com");
+			Contact person2 = new Contact("Rishi","R","Chitrudurga","KA",98,9736,"rishi@yahoo.com");
+			Contact person3 = new Contact("Gawrav","G","Durga","KA",47,7620,"gwarav@edu.com");
 		    personDetails[0]=person1;
 		    personDetails[1]=person2;
 		    personDetails[2]=person3;
